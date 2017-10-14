cfe61d61f9bfe39127b213b8b8701d65f366c09d
Nikita Kondratev <highflyer16@yandex.ru>
2017-10-14 16:21:24 +0300

    Feature. Implement evaluation with Tiny Face Detector.

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 from multiprocessing import Pool
+import os
+import time
+import argparse
+import pickle
+
 import mxnet as mx
 import cv2
+
 from mtcnn.core.symbol import P_Net, R_Net, O_Net
 from mtcnn.core.detector import Detector
 from mtcnn.core.fcn_detector import FcnDetector
 from mtcnn.tools.load_model import load_param
 from mtcnn.core.MtcnnDetector import MtcnnDetector
-import os
-import time
-import argparse
+
+import matlab.engine
+
+eng = matlab.engine.start_matlab()
+
 from utils import calc_total_score, calc_img_score
-import pickle
 
 
+# MTCNN functions
 def init_mtcnn_detector(prefix=None, epoch=None, batch_size=None, ctx=None,
                         thresh=None, min_face_size=12,
                         stride=2, slide_window=False):
@@ -55,12 +63,11 @@ def init_mtcnn_detector(prefix=None, epoch=None, batch_size=None, ctx=None,
                          stride=stride, threshold=thresh, slide_window=slide_window)
 
 
-def apply_mtcnn_to_image(filename, root_folder, mtcnn_detector = None, save_processed_image = False):
+def apply_mtcnn_to_image(filename, root_folder, mtcnn_detector=None, save_processed_image=False):
     print('[{}] Apply MTCNN to image...'.format(filename))
     if mtcnn_detector is None:
         mtcnn_detector = init_mtcnn_detector()
     img = cv2.imread(os.path.join(root_folder, filename))
-
 
     t1 = time.time()
 
@@ -99,7 +106,6 @@ def apply_mtcnn_to_image(filename, root_folder, mtcnn_detector = None, save_proc
 
 def eval_mtcnn(input_folder):
     pool = Pool()
-    # mtcnn_detector = init_mtcnn_detector()
     all_images = []
     for sub_folder in os.listdir(input_folder):
         all_images += [(os.path.join(sub_folder, x), input_folder, None) for x in
@@ -111,25 +117,54 @@ def eval_mtcnn(input_folder):
         print('Finish')
 
 
+# Tiny Face Detector functions
+def apply_tiny_fd_to_image(filename, root_folder):
+    boxes_c = eng.tiny_fd(os.path.join(root_folder, filename))
+    res = filename + '\n'
+    faces_count = 0
+    if boxes_c is not None:
+        faces_count = len(boxes_c)
+        for b in boxes_c:
+            res += '{} {} {} {}\n'.format(int(b[0]), int(b[1]), int(b[2]), int(b[3]))
+    print('[{}] Founded {} boxes. Finish.'.format(filename, faces_count))
+    return res
+
+
+def eval_tiny_fd(input_folder):
+    pool = Pool()
+    all_images = []
+    for sub_folder in os.listdir(input_folder):
+        all_images += [(os.path.join(sub_folder, x), input_folder, None) for x in
+                       os.listdir(os.path.join(input_folder, sub_folder))]
+    res = pool.starmap(apply_tiny_fd_to_image, all_images)
+    output_filename = '{}_tiny_fd_results.txt'.format(input_folder)
+    with open(output_filename, 'w') as f:
+        f.write(''.join(res))
+        print('Finish')
+
+
 def parse_args():
-    parser = argparse.ArgumentParser(description='MTCNN evaluation on folder')
+    parser = argparse.ArgumentParser(description='Evaluation on folder')
     parser.add_argument('--folder', type=str, default='widerface')
     parser.add_argument('--report_filename', type=str, default='report.txt')
+    parser.add_argument('--model', type=str, default='mtcnn')
+    parser.add_argument('--gt_filename', type=str, default='wider_face_val_bbx_gt._transformed.txt')
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = parse_args()
-    # MTCNN model evaluation
+    # Model evaluation
     t1 = time.time()
-    print('Start evaluation MTCNN on folder {}'.format(args.folder))
-    eval_mtcnn(args.folder)
-    mtcnn_time = time.time() - t1
-    print('Total time: ', mtcnn_time)
-    mtcnn_results_filename = '{}_mtcnn_results.txt'.format(args.folder)
-    truth_results_filename = 'wider_face_val_bbx_gt._transformed.txt'
-    mtcnn_score = calc_total_score(mtcnn_results_filename, truth_results_filename)
+    print('Start evaluation {} on folder {}'.format(args.model, args.folder))
+    if args.model == 'mtcnn':
+        eval_mtcnn(args.folder)
+    elif args.model == 'tiny_fd':
+        eval_tiny_fd(args.folder)
+    execution_time = time.time() - t1
+    print('Total time: ', execution_time)
+    results_filename = '{}_{}_results.txt'.format(args.folder, args.model)
+    score = calc_total_score(results_filename, args.gt_filename)
     with open(args.report_filename, 'w') as rf:
-        rf.write('==== MTCNN results ====\nExecution time: {} seconds\nTotal score: {}\n'.format(mtcnn_time, mtcnn_score))
-
-
+        rf.write('==== {} results ====\nExecution time: {} seconds\nTotal score: {}\n'
+                 .format(args.model, execution_time, score))
